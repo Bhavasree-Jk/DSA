ad130984f3a6e20cc5a33f932cc0cddaf78d729f
Bhavasree_J_K <bhavasree025@gmail.com>
2025-07-02 14:30:45 +0530

    Time: 9 ms (26.23%) | Memory: 43.4 MB (15.05%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,12 +1,10 @@
-
 import java.math.BigInteger;
-
-
 class Solution {
     public String multiply(String num1, String num2) {
-        BigInteger b = new BigInteger(num1);
-        BigInteger k = new BigInteger(num2);
-        BigInteger result = b.multiply(k);  
-        return result.toString();   
+     BigInteger b= new BigInteger(num1);
+      BigInteger k= new BigInteger(num2);
+      BigInteger ans=b.multiply(k);
+      return ans.toString();
+
     }
 }
